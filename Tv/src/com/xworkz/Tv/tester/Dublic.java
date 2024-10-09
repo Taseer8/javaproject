2e68432d7542de1b3721655259281e91919d4e59
Taseer8 <taseer08.xworkz@gmail.com>
2024-10-09 20:20:27 +0530

    dublicate char in String

---
+++ Tv/src/com/xworkz/Tv/tester/Dublic.java
@@ -0,0 +1,21 @@
+package com.xworkz.Tv.tester;
+
+public class Dublic {
+
+	public static void main(String[] args) {
+		String name = "mohammed taseer";
+		char ch[] = name.toCharArray();
+		int count =0;
+		for(int i = 0; i<ch.length; i++) {
+			for(int j = i+1; j<ch.length; j++) {
+				if(ch[i]== ch[j]) {
+					System.out.println("doublicate characters is :"+ch[j]);
+					count++;
+				}
+			}
+		}
+		System.out.println("no of doublicate characters persent is :"+count);
+				
+	}
+
+}
