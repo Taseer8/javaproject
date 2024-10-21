ec613eadfb3c4aec5b2e030118b79e88613497aa
Taseer8 <taseer08.xworkz@gmail.com>
2024-10-21 12:20:04 +0530

    collection -> itrator

---
+++ Collection/src/com/xworkz/collection/Task.java
@@ -0,0 +1,22 @@
+package com.xworkz.collection;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Task {
+
+	public static void main(String[] args) {
+		System.out.println("main");
+		List<String> list = new LinkedList<String>();
+		list.add("hi well come to xworkz");
+		Iterator<String> itrator = list.iterator();
+	
+		
+		itrator.remove();
+		while (itrator.hasNext()) {
+			System.out.println(itrator.next());
+		}
+	}
+
+}
